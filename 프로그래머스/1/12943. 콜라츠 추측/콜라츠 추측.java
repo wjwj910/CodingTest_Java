f63190ff48d91d657162ec408e5056f603e2363b
이원재 <101052153+wjwj910@users.noreply.github.com>
2025-11-21 13:08:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        int count = 0;
+        
+        while(num != 1){
+            if(count == 500){
+                count = -1;
+                break;
+            }else if(num == 0){
+                break;
+            }else if(num%2 == 0){
+                num /= 2;
+            }else if(num%2 == 1){
+                num = num*3 + 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
